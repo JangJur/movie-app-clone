ff4fe6ec7b37f494a68109d97d97cc562dcfb31a
JangJur <wkdwndud753@naver.com>
2018-07-30 03:44:02 -0700

    Component2
    props
    -Parent component give data to children componet.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Moive.js'
 
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Star Wars"
+]
+
+const movieImage = [
+  "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg",
+  "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie title={movieTitles[0]} poster={movieImage[0]} />
+        <Movie title={movieTitles[1]} poster={movieImage[1]} />
+        <Movie title={movieTitles[2]} poster={movieImage[2]} />
+        <Movie title={movieTitles[3]} poster={movieImage[3]} />
       </div>
     );
   }
