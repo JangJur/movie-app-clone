c10b9c66e9e0972c338bd527dd61640e9f27d05b
JangJur <wkdwndud753@naver.com>
2018-07-31 02:51:04 -0700

    Ajax(AJAJSON)
    Ajax
    - Asynchronous JavaScript and Xml
    
    JSON
    - JavaScript Object Notation
    
    fetch request

--- src/App.js
+++ src/App.js
@@ -4,36 +4,10 @@ import Movie from './Moive.js'
 
 class App extends Component {
 
-  state = {
-  }
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "http://www.impawards.com/2013/posters/oldboy_ver3.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
-          },
-          {
-            title: "Trainsporting",
-            poster: "http://dazedimg.dazedgroup.netdna-cdn.com/1600/azure/dazed-prod/1190/2/1192408.jpg"
-          }
-        ]
-      })
-    }, 5000)
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'))
   }
 
   _renderMovies = () => {
