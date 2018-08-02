ca7efa8e7c6d16227d8d31c6fff50808542a4ec0
JangJur <wkdwndud753@naver.com>
2018-08-02 09:03:51 -0700

    Production
    Deploying a Create React App to Github pages

--- src/Moive.js
+++ src/Moive.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
 function Movie({title, poster, genres, synopsis}) {
     return (
         <div className="Movie">
-            <div className="Movie_Colums">
-            <MoviePoster poster={poster} alt={title} />
+            <div className="Movie_Column">
+                <MoviePoster poster={poster} alt={title} />
             </div>
-            <div className="Movie_Colums">
+            <div className="Movie_Column">
                 <h1>{title}</h1>
                 <div className="Movie_Genres">
                     {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
                 </div>
-                <p className="Moive_Synopsis">
-                {synopsis}
-                </p>
+                <div className="Movie_Synopsis">
+                <LinesEllipsis
+                    text={synopsis}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    />   
+                </div>
             </div>
         </div>
     )
