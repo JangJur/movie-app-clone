cb2a8d09c66d1ff9a6b9e66d9e28828964372f5c
JangJur <wkdwndud753@naver.com>
2018-08-02 05:20:08 -0700

    Updating_Movie_Component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie
+      title={movie.title_english}
+      poster={movie.medium_cover_image}
+      key={movie.id}
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
     })
     return movies
   }
@@ -25,7 +31,7 @@ class App extends Component {
   }
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=ratig')
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
     .then(potato => potato.json())
     .then(json => json.data.movies)
     .catch(err => console.log(err))
